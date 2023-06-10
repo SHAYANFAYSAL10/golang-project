14d6e89c9b7af651da351d861d96e23a2c34241a
SHAYANFAYSAL10 <116728630+SHAYANFAYSAL10@users.noreply.github.com>
2023-06-10 22:04:12 +0600

    deleted comment

--- main.go
+++ main.go
@@ -8,30 +8,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// import (
-// 	"database/sql"
-// 	"fmt"
-
-// 	_ "github.com/go-sql-driver/mysql"
-// )
-
-// func main() {
-// 	db, err := sql.Open("mysql","root:1703179Faysal@tcp(localhost:3306)/user_activity")
-// 	if err !=nil {
-// 		fmt.Println("Error")
-// 		panic(err.Error())
-// 	}
-// 	defer db.Close()
-
-// 	err = db.Ping()
-// 	if err != nil {
-// 		fmt.Println("Error")
-// 		panic(err.Error())
-// 	}
-
-// 	fmt.Println("Connected")
-// }
-
 func main() {
 	router := mux.NewRouter()
 
